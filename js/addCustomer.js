bb15edf5cc861fea6b358b7230c32faf6264e390
Niall <noccer@gmail.com>
2016-07-04 14:32:43 +1000

    NOC: added in logic so that when visting 'domain/nialls-cafe-richmond' the customers are pulled in for that particular restaurant.

--- js/addCustomer.js
+++ js/addCustomer.js
@@ -1,61 +1,74 @@
 $(document).ready(function() {
 
-   $.getJSON('http://localhost:3000/5776194a5260e7e484bbc080/index', function(data) {
+  var restaurantNameSuburb = "";
+    console.log(window.location);
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+  restaurantNameSuburb = getParameterByName('r_id');
+  console.log(restaurantNameSuburb);
+  $.getJSON('http://localhost:3000/'+restaurantNameSuburb+'/index', function(data) {
 
-      $.each(data, function(i) {
-       var customerRef = data[i];
+    $.each(data, function(i) {
+      var customerRef = data[i];
 
-         var customerRow = $('<tr>').addClass('customer-row');
-             var customerHeads = $('<td>').addClass('heads')
-              .attr('width', '10px')
-              .text(customerRef.heads);
-             var customerName = $('<td>')
-              .attr('width', '300px')
-              .addClass('name')
-              .text(customerRef.customerName);
-              // insert time tracking
-              var now = new Date();
-              var momentNow = moment(now);
-              var waitingArray = [];
-              $.each(customerRef, function() {
-                var delta =
-                  (moment(customerRef.finishedWaiting).valueOf() - momentNow.valueOf()) / 60000;
-                waitingArray.push(delta);
-              });
-              var eta = Math.max.apply(Math, waitingArray);
-              if (eta < 1 && data[i].length !== 0) {
-                eta = "Due";
-              } else if (data[i].length === 0) {
-                eta = "--";
-              }
-              console.log(waitingArray);
-              console.log(eta);
-              var customerTimer = $('<td>')
-              .attr('width', '10px')
-              .text(eta)
-              .addClass('timer');
-              // end time tracking
+      var customerRow = $('<tr>').addClass('customer-row');
+      var customerHeads = $('<td>').addClass('heads')
+        .attr('width', '10px')
+        .text(customerRef.heads);
+      var customerName = $('<td>')
+        .attr('width', '300px')
+        .addClass('name')
+        .text(customerRef.customerName);
+      // insert time tracking
+      var now = new Date();
+      var momentNow = moment(now);
+      var waitingArray = [];
+      $.each(customerRef, function() {
+        var delta =
+          (moment(customerRef.finishedWaiting).valueOf() - momentNow.valueOf()) / 60000;
+        waitingArray.push(delta);
+      });
+      var eta = Math.max.apply(Math, waitingArray);
+      if (eta < 1 && data[i].length !== 0) {
+        eta = "Due";
+      } else if (data[i].length === 0) {
+        eta = "--";
+      }
+      console.log(waitingArray);
+      console.log(eta);
+      var customerTimer = $('<td>')
+        .attr('width', '10px')
+        .text(eta)
+        .addClass('timer');
+      // end time tracking
 
-            var formTd = $('<td>');
-              var delayForm = $('<form>').addClass('delayForm flex');
-                var customerDelayField = $('<input>')
-                  .attr('type', 'number')
-                  .attr('placeholder', '#');
-                var customerDelayBtn = $('<button>')
-                  .addClass('btn-remove')
-                  .text('Do it');
-              var formComplete = $(delayForm).append(customerDelayField, customerDelayBtn);
-            $(formTd).append(formComplete);
+      var formTd = $('<td>');
+      var delayForm = $('<form>').addClass('delayForm flex');
+      var customerDelayField = $('<input>')
+        .attr('type', 'number')
+        .attr('placeholder', '#');
+      var customerDelayBtn = $('<button>')
+        .addClass('btn-remove')
+        .text('Do it');
+      var formComplete = $(delayForm).append(customerDelayField, customerDelayBtn);
+      $(formTd).append(formComplete);
 
-            var customerEditRemoveBtnTd = $('<td>').addClass('flex');
-              var customerRemoveBtn = $('<button>')
-                 .addClass('btn-remove')
-                 .css('margin-left', '5px')
-                 .text('Remove');
-              var customerEditBtn = $('<button>')
-                 .addClass('btn-remove')
-                 .text('Edit');
-              $(customerEditRemoveBtnTd).append(customerEditBtn, customerRemoveBtn);
+      var customerEditRemoveBtnTd = $('<td>').addClass('flex');
+      var customerRemoveBtn = $('<button>')
+        .addClass('btn-remove')
+        .css('margin-left', '5px')
+        .text('Remove');
+      var customerEditBtn = $('<button>')
+        .addClass('btn-remove')
+        .text('Edit');
+      $(customerEditRemoveBtnTd).append(customerEditBtn, customerRemoveBtn);
 
       $(customerRow).append(
         customerHeads,
@@ -67,8 +80,8 @@ $(document).ready(function() {
 
       $('#customerListAdmin').append(customerRow);
 
-      });
-   });
+    });
+  });
 
 //
 // function addCustomerToDomFromForm() {
