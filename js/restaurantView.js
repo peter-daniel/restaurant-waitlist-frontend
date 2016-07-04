958daa0386e87874042fe39832c04c24b95c5f7e
Niall <noccer@gmail.com>
2016-07-04 14:35:59 +1000

    NOC: added in new file that allows us to get the query from the url so we can pass the same piece of info between pages.

---
+++ js/restaurantView.js
@@ -0,0 +1,18 @@
+// this file allows us to obtain the current restaurantNameSuburb 'id' and use it
+
+$(function(){
+  console.log(window.location);
+
+  function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  var restaurantNameSuburb = getParameterByName('r_id');
+
+});
